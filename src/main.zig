7d2fefd7a50ea52f2166f25486fd8f7a78f06ae9
vaelixd <vaelixd@proton.me>
2025-12-14 10:06:37 +1300

    pass source into Lexer and Parser

--- src/main.zig
+++ src/main.zig
@@ -12,15 +12,16 @@ pub fn main() !void {
     defer argv.deinit();
     _ = argv.next();
     const filename = if (argv.next()) |a| std.mem.sliceTo(a, 0) else "main.wp";
+    const src = try lexer.readFile(alloc, filename);
 
-    var tokens = try lexer.lex(alloc, filename);
+    var tokens = try lexer.lex(alloc, filename, src);
     if (tokens == .err) {
         const err = tokens.err;
         std.debug.print("{s}\n", .{err.message});
         std.process.exit(err.code);
     }
 
-    const tree = try ast.parse(alloc, filename, &tokens.ok);
+    const tree = try ast.parse(alloc, filename, src, &tokens.ok);
     if (tree == .err) {
         const err = tree.err;
         std.debug.print("{s}\n", .{err.message});
