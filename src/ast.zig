af1bb18a1307de79c3e456ab04b5970494149e8d
vaelixd <vaelixd@proton.me>
2025-12-13 22:30:35 +1300

    add skeleton for AST parsing

--- src/ast.zig
+++ src/ast.zig
@@ -1,6 +1,37 @@
 const std = @import("std");
 const types = @import("./types.zig");
 
-pub fn parse(alloc: std.mem.Allocator) !types.astNode {
-    _ = alloc;
+pub fn parse(alloc: std.mem.Allocator, filename: []const u8, tokens: *types.TokenIterator) !types.Result(types.AST) {
+    var nodes: std.ArrayList(types.AstNode) = .empty;
+
+    while (tokens.next()) |token| {
+        var node: types.AstNode = .{};
+
+        if (token.kind == .Word) {
+            node.kind = .Id;
+            node.value = token.value;
+        } else {
+            return .{
+                .err = .{
+                    .message = try std.fmt.allocPrint(
+                        alloc,
+                        "unexpected token: {s}:{d}:{d} {c}",
+                        .{
+                            filename,
+                            0,
+                            0,
+                            types.TokKind.kindToChar(token.kind),
+                        },
+                    ),
+                    .code = 1,
+                },
+            };
+        }
+    }
+
+    return .{
+        .ok = .{
+            .nodes = try nodes.toOwnedSlice(alloc),
+        },
+    };
 }
