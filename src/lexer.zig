02919be222b35b8b99161d1a51493df505969666
vaelixd <vaelixd@proton.me>
2025-12-15 09:11:03 +1300

    reorganize and handle strings

--- src/lexer.zig
+++ src/lexer.zig
@@ -16,12 +16,18 @@ pub fn lex(alloc: std.mem.Allocator, filename: [:0]const u8, src: [][]u8) !error
             if (std.ascii.isWhitespace(char)) continue;
             if (std.mem.startsWith(u8, line[col..], "//")) break;
 
-            if (types.TokKind.charToKind(char)) |kind| {
+            if (std.ascii.isAlphabetic(char) or char == '_') {
+                const start = col;
+                while (col < line.len and
+                    (std.ascii.isAlphanumeric(line[col]) or line[col] == '_')) : (col += 1)
+                {}
+
+                col -= 1;
                 token = .{
-                    .kind = kind,
-                    .value = line[col .. col + 1],
+                    .kind = .Word,
+                    .value = line[start .. col + 1],
                     .line_num = line_num,
-                    .line_col = col,
+                    .line_col = start,
                     .line_col_end = col,
                 };
             } else if (std.ascii.isDigit(char)) {
@@ -30,7 +36,33 @@ pub fn lex(alloc: std.mem.Allocator, filename: [:0]const u8, src: [][]u8) !error
                 var num_kind: numbers.NumberKind = .DecimalInt;
                 const start = col;
                 while (col < line.len) : (col += 1) {
-                    if (!numbers.validChar(num_kind, char)) return .{
+                    if (std.ascii.isWhitespace(line[col])) break;
+                    switch (line[col]) {
+                        '+', '-', '*', '/', '%', '^' => break,
+                        '.' => if (col + 1 < line.len) {
+                            switch (line[col + 1]) {
+                                '&', '|', '^', '<', '>', '!' => break,
+                                else => {},
+                            }
+                        },
+                        else => {},
+                    }
+
+                    if (col == start + 1 and line[start] == '0') {
+                        switch (line[col]) {
+                            'x' => num_kind = .HexInt,
+                            'b' => num_kind = .BinaryInt,
+                            else => {},
+                        }
+
+                        continue;
+                    }
+
+                    if (num_kind == .DecimalInt and numbers.char_kind[line[col]].decimal_point) num_kind = .DecimalFloat;
+                    if (num_kind == .HexInt and numbers.char_kind[line[col]].decimal_point) num_kind = .HexFloat;
+                    if (num_kind == .BinaryInt and numbers.char_kind[line[col]].decimal_point) num_kind = .BinaryFloat;
+
+                    if (!numbers.validChar(num_kind, line[col])) return .{
                         .err = .{ .message = try errors.format(
                             alloc,
                             "invalid character in number",
@@ -42,10 +74,6 @@ pub fn lex(alloc: std.mem.Allocator, filename: [:0]const u8, src: [][]u8) !error
                             col,
                         ), .code = 1 },
                     };
-
-                    if (num_kind == .DecimalInt and numbers.char_kind[char].decimal_point) num_kind = .DecimalFloat;
-                    if (num_kind == .HexInt and numbers.char_kind[char].decimal_point) num_kind = .HexFloat;
-                    if (num_kind == .BinaryInt and numbers.char_kind[char].decimal_point) num_kind = .BinaryFloat;
                 }
 
                 const underscore_error = numbers.validateUnderscores(line, start, col);
@@ -71,18 +99,97 @@ pub fn lex(alloc: std.mem.Allocator, filename: [:0]const u8, src: [][]u8) !error
                     .line_col = start,
                     .line_col_end = col,
                 };
-            } else if (std.ascii.isAlphabetic(char) or char == '_') {
+            } else if (isQuote(char)) {
+                const quote = char;
                 const start = col;
-                while (col < line.len and
-                    (std.ascii.isAlphanumeric(line[col]) or line[col] == '_')) : (col += 1)
-                {}
 
-                col -= 1;
+                col += 1;
+                while (col < line.len) : (col += 1) {
+                    if (line[col] == '\\' and col + 1 < line.len and isQuote(line[col + 1])) {
+                        col += 1;
+                        continue;
+                    }
+
+                    if (line[col] == quote) break;
+                }
+
+                if (col == line.len) {
+                    return .{
+                        .err = .{
+                            .message = try errors.format(
+                                alloc,
+                                "expected closing quote",
+                                filename,
+                                line,
+                                null,
+                                line_num,
+                                col,
+                                col,
+                            ),
+                            .code = 1,
+                        },
+                    };
+                }
+
                 token = .{
-                    .kind = .Word,
-                    .value = line[start .. col + 1],
+                    .kind = .String,
+                    .value = line[start + 1 .. col],
                     .line_num = line_num,
                     .line_col = start,
+                    .line_col_end = col,
+                };
+            } else if (char == '.') {
+                var kind: types.TokKind = .BAnd;
+
+                if (col + 1 < line.len) {
+                    switch (line[col + 1]) {
+                        '&' => kind = .BAnd,
+                        '|' => kind = .BOr,
+                        '^' => kind = .BXor,
+                        '<' => kind = .BLeft,
+                        '>' => kind = .BRight,
+                        '!' => kind = .BNot,
+                        else => return .{
+                            .err = .{ .message = try errors.format(
+                                alloc,
+                                "unrecognized bitwise operator",
+                                filename,
+                                line,
+                                line[col .. col + 2],
+                                line_num,
+                                col,
+                                col + 2,
+                            ), .code = 1 },
+                        },
+                    }
+                } else {
+                    return .{
+                        .err = .{ .message = try errors.format(
+                            alloc,
+                            "expected second symbol after bitwise initializer",
+                            filename,
+                            line,
+                            line[col .. col + 1],
+                            line_num,
+                            col,
+                            col,
+                        ), .code = 1 },
+                    };
+                }
+
+                token = .{
+                    .kind = kind,
+                    .value = line[col .. col + 2],
+                    .line_num = line_num,
+                    .line_col = col,
+                    .line_col_end = col + 1,
+                };
+            } else if (types.TokKind.charToKind(char)) |kind| {
+                token = .{
+                    .kind = kind,
+                    .value = line[col .. col + 1],
+                    .line_num = line_num,
+                    .line_col = col,
                     .line_col_end = col,
                 };
             } else {
@@ -139,4 +246,11 @@ pub fn readFile(alloc: std.mem.Allocator, filename: [:0]const u8) ![][]u8 {
     }
 
     return lines.toOwnedSlice(alloc);
+}
+
+fn isQuote(char: u8) bool {
+    switch (char) {
+        '\'', '"', '`' => return true,
+        else => return false,
+    }
 }
