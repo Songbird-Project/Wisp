9a170131c89325ef37043e47047643096102d513
vaelixd <vaelixd@proton.me>
2025-12-19 10:59:43 +1300

    use `parseExpr` where required

--- src/ast.zig
+++ src/ast.zig
@@ -11,7 +11,7 @@ pub fn parse(alloc: std.mem.Allocator, filename: []const u8, src: [][]u8, tokens
 
         switch (token.kind) {
             .Hash => {
-                node = parseDirective(tokens);
+                node = try parseDirective(alloc, tokens);
             },
             .Fn => {
                 // TODO: parseFn
@@ -20,28 +20,13 @@ pub fn parse(alloc: std.mem.Allocator, filename: []const u8, src: [][]u8, tokens
                 // TODO: parseReturn
             },
             .Exit => {
-                node = parseExit(tokens);
-            },
-            .Id => {
-                const id = tokens.peek(1) orelse break;
-                switch (id.kind) {
-                    .ColonColon => {
-                        // TODO: parseType
-                    },
-                    .Period => {
-                        // TODO: parseCall
-                    },
-                    .ColonEqual, .HashEqual, .Equals => {
-                        // TODO: parseAssign
-                    },
-                    else => {},
-                }
+                node = try parseExit(alloc, tokens);
             },
             else => {},
         }
 
         if (node == null) {
-            node = parseExpr(tokens, 0);
+            node = try parseExpr(alloc, tokens, 0);
         }
 
         if (node.? == .err) {
@@ -93,13 +78,13 @@ pub fn parse(alloc: std.mem.Allocator, filename: []const u8, src: [][]u8, tokens
     };
 }
 
-fn parseExpr(tokens: *types.TokenIterator, min_bp: u8) types.ParserReturn {
+fn parseExpr(alloc: std.mem.Allocator, tokens: *types.TokenIterator, min_bp: u8) !types.ParserReturn {
     const lhs_tok = tokens.next().?;
     var lhs: types.ASTNode = .Nil;
     var rhs: types.ASTNode = .Nil;
 
     if (prefixBP(lhs_tok.kind)) |bp| {
-        const expr = parseExpr(tokens, bp);
+        const expr = try parseExpr(alloc, tokens, bp);
         if (expr == .err) return .{ .err = expr.err };
         rhs = expr.ok;
         const kind: types.ASTKind = switch (lhs_tok.kind) {
@@ -131,7 +116,7 @@ fn parseExpr(tokens: *types.TokenIterator, min_bp: u8) types.ParserReturn {
             else => unreachable,
         };
     } else if (lhs_tok.kind == .LBracket) {
-        const expr = parseExpr(tokens, 0);
+        const expr = try parseExpr(alloc, tokens, 0);
         if (expr == .err) return .{ .err = expr.err };
         lhs = expr.ok;
         if (tokens.expect(.RBracket) == null) {
@@ -153,37 +138,65 @@ fn parseExpr(tokens: *types.TokenIterator, min_bp: u8) types.ParserReturn {
         };
     }
 
-    while (tokens.peek(0)) |next_tok| {
-        if (postfixBP(next_tok.kind)) |bp| {
+    while (tokens.peek(0)) |tok| {
+        if (postfixBP(tok.kind)) |bp| {
             if (bp < min_bp) break;
             _ = tokens.next();
 
-            const kind: types.ASTKind = switch (next_tok.kind) {
-                .PlusPlus => .Increment,
-                .DashDash => .Decrement,
-                else => unreachable,
-            };
+            if (tok.kind == .LBracket) {
+                var args: std.ArrayList(*types.ASTNode) = .empty;
+                while (tokens.peek(0) != null and tokens.peek(0).?.kind != .Newline and tokens.peek(0).?.kind != .RBracket) {
+                    var expr = try parseExpr(alloc, tokens, 0);
+                    if (expr == .err) return .{ .err = expr.err };
+                    try args.append(alloc, &expr.ok);
+                    if (tokens.peek(0).?.kind == .Comma) _ = tokens.next();
+                }
 
-            lhs = .{
-                .PostfixOp = .{
-                    .kind = kind,
-                    .value = &lhs,
-                },
-            };
+                if (tokens.expect(.RBracket) == null) {
+                    return .{
+                        .err = .{
+                            .token = tokens.tokens[tokens.index - 1],
+                            .message = "expected closing bracket after arguments",
+                            .code = 1,
+                        },
+                    };
+                }
+
+                lhs = .{
+                    .Call = .{
+                        .callee = &lhs,
+                        .args = try args.toOwnedSlice(alloc),
+                    },
+                };
+            } else {
+                const kind: types.ASTKind = switch (tok.kind) {
+                    .PlusPlus => .Increment,
+                    .DashDash => .Decrement,
+                    else => unreachable,
+                };
+
+                lhs = .{
+                    .PostfixOp = .{
+                        .kind = kind,
+                        .value = &lhs,
+                    },
+                };
+            }
 
             continue;
         }
 
-        if (infixBP(next_tok.kind)) |bps| {
+        if (infixBP(tok.kind)) |bps| {
             if (bps.left < min_bp) break;
 
             _ = tokens.next();
-            const expr = parseExpr(tokens, bps.right);
+
+            const expr = try parseExpr(alloc, tokens, bps.right);
             if (expr == .err) {
                 return .{ .err = expr.err };
             }
             rhs = expr.ok;
-            const kind: types.ASTKind = switch (next_tok.kind) {
+            const kind: types.ASTKind = switch (tok.kind) {
                 .Equals => .Reassign,
                 .ColonEqual => .AssignVar,
                 .HashEqual => .AssignConst,
@@ -225,7 +238,7 @@ fn parseExpr(tokens: *types.TokenIterator, min_bp: u8) types.ParserReturn {
     return .{ .ok = lhs };
 }
 
-fn parseDirective(tokens: *types.TokenIterator) types.ParserReturn {
+fn parseDirective(alloc: std.mem.Allocator, tokens: *types.TokenIterator) !types.ParserReturn {
     var node: types.ASTNode = undefined;
     const hash = tokens.next().?;
 
@@ -251,7 +264,7 @@ fn parseDirective(tokens: *types.TokenIterator) types.ParserReturn {
     }
 
     if (std.mem.eql(u8, name.?.value, "import")) {
-        const import = parseImport(tokens);
+        const import = try parseImport(alloc, tokens);
         if (import == .err) return .{ .err = import.err };
         node = import.ok;
     } else {
@@ -277,12 +290,12 @@ fn parseDirective(tokens: *types.TokenIterator) types.ParserReturn {
     return .{ .ok = node };
 }
 
-fn parseImport(tokens: *types.TokenIterator) types.ParserReturn {
-    const path = tokens.next();
-    if (path == null) {
+fn parseImport(alloc: std.mem.Allocator, tokens: *types.TokenIterator) !types.ParserReturn {
+    var path = try parseExpr(alloc, tokens, 0);
+    if (path == .err) {
         return .{
             .err = .{
-                .token = tokens.tokens[tokens.index - 1],
+                .token = path.err.token,
                 .message = "expected library or file in import directive",
                 .code = 1,
             },
@@ -290,11 +303,11 @@ fn parseImport(tokens: *types.TokenIterator) types.ParserReturn {
     }
 
     return .{
-        .ok = .{ .Import = path.?.value },
+        .ok = .{ .Import = &path.ok },
     };
 }
 
-fn parseExit(tokens: *types.TokenIterator) types.ParserReturn {
+fn parseExit(alloc: std.mem.Allocator, tokens: *types.TokenIterator) !types.ParserReturn {
     _ = tokens.next();
 
     var immediate_exit: bool = true;
@@ -327,20 +340,10 @@ fn parseExit(tokens: *types.TokenIterator) types.ParserReturn {
             };
         }
 
-        var value_node: types.ASTNode = switch (value.?.kind) {
-            .Number => .{
-                .Number = .{
-                    .kind = numbers.NumberKind.DecimalInt,
-                    .value = value.?.value,
-                },
-            },
-            .Id => .{
-                .Identifier = value.?.value,
-            },
-            else => unreachable,
-        };
+        var value_node = try parseExpr(alloc, tokens, 0);
+        if (value_node == .err) return .{ .err = value_node.err };
 
-        exit_value = &value_node;
+        exit_value = &value_node.ok;
     }
 
     return .{
@@ -373,16 +376,17 @@ fn infixBP(kind: types.TokKind) ?struct { left: u8, right: u8 } {
 
 fn prefixBP(kind: types.TokKind) ?u8 {
     return switch (kind) {
-        .ColonColon => 14,
-        .Dash, .Bang, .BNot => 15,
-        .PlusPlus, .DashDash => 16,
+        .ColonColon => 15,
+        .Dash, .Bang, .BNot => 16,
+        .PlusPlus, .DashDash => 17,
         else => null,
     };
 }
 
 fn postfixBP(kind: types.TokKind) ?u8 {
     return switch (kind) {
-        .PlusPlus, .DashDash => 17,
+        .PlusPlus, .DashDash => 18,
+        .LBracket => 19,
         else => null,
     };
 }
